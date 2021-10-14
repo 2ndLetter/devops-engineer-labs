f82fad52ffb0afdd3815328c45882413543f8d89
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-10-13 21:25:22 -0400

    updated script

--- cfn-ec2.sh
+++ cfn-ec2.sh
@@ -2,13 +2,15 @@
 
 # Create keypair
 KEYPAIR=$(aws ec2 describe-key-pairs --filters Name=key-name,Values=keypair | jq --raw-output .KeyPairs[].KeyName)
-
 if [[ $KEYPAIR == keypair ]]
 then
-  rm -fr CFN/keypair.pem
-  aws ec2 delete-key-pair --key-name keypair
-  aws ec2 create-key-pair --key-name keypair --query "KeyMaterial" --output text > CFN/keypair.pem
+  echo "keypair already exist!"
+  #rm -fr CFN/keypair.pem
+  #aws ec2 delete-key-pair --key-name keypair
+  #aws ec2 create-key-pair --key-name keypair --query "KeyMaterial" --output text > CFN/keypair.pem
 else
+  echo "creating keypair!"
+  rm -fr CFN/keypair.pem
   aws ec2 create-key-pair --key-name keypair --query "KeyMaterial" --output text > CFN/keypair.pem
 fi
 
