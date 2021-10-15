335ed2807f3071922bdef343fd4c90eca4ae8251
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-10-15 13:22:13 -0400

    added a GH script to delete all versioned objects

--- delete-codedeploy.sh
+++ delete-codedeploy.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-# Delete CodeDeploy deployment group
+# Declare the BUCKET var
+BUCKET=$(aws s3 ls | grep "aws-devops-course" | cut -d " " -f 3)
+
+# Delete all versions of objects in bucket
+# Source: https://github.com/linz/geostore/blob/19dba64cce0c86b1f4d0bb66bb8246c149a61a01/clear-s3-buckets.bash
+./delete-s3-buckets.sh $BUCKET
+
+# Delete S3 bucket
+aws s3 rb s3://$BUCKET --force
 
 # Delete CodeDeploy Application
+aws deploy delete-application --application-name CodeDeployDemo
