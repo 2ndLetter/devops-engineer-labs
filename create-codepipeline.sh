eb86325b053ea43c0bea586b5ee7901b3320bc0e
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-10-16 09:03:29 -0400

    add codepipeline stuff

--- create-codepipeline.sh
+++ create-codepipeline.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -ex
 
-# Create codepipeline pipeline
+# Create codepipeline pipeline template
 export ACCOUNT=$(aws sts get-caller-identity | jq -r .Account)
 export BUCKET=$(aws s3 ls | grep "aws-devops-course" | cut -d " " -f 3)
-#aws codepipeline create-pipeline --cli-input-json file://codepipeline-project.json
+sed -e "s/ACCOUNT_VAR/$ACCOUNT/g" -e "s/BUCKET_VAR/$BUCKET/g" codepipeline-project-source.json > codepipeline-project.json
 
-cat codepipeline-project.json
+
+# Create codepipeline pipeline
+#aws codepipeline create-pipeline --cli-input-json file://codepipeline-project.json
